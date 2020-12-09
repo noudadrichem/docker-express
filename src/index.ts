30c779453ca4b39c5d2b287a34d4c05d041cd9a5
noudadrichem <info@noudadrichem.com>
2020-12-09 23:08:58 +0100

    init

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import 'dotenv/config'
+import express from 'express'
+const app = express()
+const port = process.env.PORT || 5000
+app.get('/', (_, res) => {
+  console.log('GET / ')
+  res.status(200).json({
+    message: 'success'
+  })
+})
+app.listen(port, () => console.log(`Running on port ${port}`))
