767e17613853a024943983b0ff6507db18c47f9b
noud adrichem <info@noudadrichem.com>
2020-12-10 20:08:22 +0100

    new workflow

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,9 @@ import express from 'express'
 const app = express()
 const port = process.env.PORT || 5000
 app.get('/', (_, res) => {
-  console.log('GET NEW / ')
+  console.log('GET NEW NEW / ')
   res.status(200).json({
-    message: 'This is a new version...'
+    message: 'This is a new new version...'
   })
 })
 app.listen(port, () => console.log(`Running on port ${port}`))
