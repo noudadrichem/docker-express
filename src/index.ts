4822aec438bb6dc69b63b0cb4196ed922a577fce
noud adrichem <info@noudadrichem.com>
2020-12-10 19:40:47 +0100

    new data

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,9 @@ import express from 'express'
 const app = express()
 const port = process.env.PORT || 5000
 app.get('/', (_, res) => {
-  console.log('GET / ')
+  console.log('GET NEW / ')
   res.status(200).json({
-    message: 'success'
+    message: 'This is a new version...'
   })
 })
 app.listen(port, () => console.log(`Running on port ${port}`))
