231b8c5d0b5aeda5ac29913e2824c6f146cf8dd4
Noud Adrichem <info@noudadrichem.com>
2020-12-29 13:39:09 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const port = process.env.PORT || 5000
 app.get('/', (_, res) => {
   console.log('GET / ')
   res.status(200).json({
-    message: 'Hello World'
+    message: 'Hello Actions'
   })
 })
 app.listen(port, () => console.log(`Running on port ${port}`))
