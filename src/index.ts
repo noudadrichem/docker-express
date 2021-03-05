a0e6d9b09b61f83306ead4bef1ace73849cc9d04
noudadrichem <info@noudadrichem.com>
2021-03-05 12:25:03 +0100

    hostname in response to see in k8s

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import 'dotenv/config'
 import express from 'express'
+import os from 'os'
+
 const app = express()
 const port = process.env.PORT || 5000
+
 app.get('/', (_, res) => {
   console.log('GET / ')
   res.status(200).json({
-    message: 'Hello Actions'
+    message: 'Hello Actions',
+    os: os.hostname
   })
 })
 app.listen(port, () => console.log(`Running on port ${port}`))
